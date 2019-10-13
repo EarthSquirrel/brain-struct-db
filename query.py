aaa2db2c48fc55e06d80ccc909f54eea895d3be2
Britney <britney.gibbs@student.montana.edu>
2019-10-12 18:17:40 -0600

    Get all networks

--- query.py
+++ query.py
@@ -7,7 +7,20 @@ def get_all_structs():
     structs = []
     for d in data:
         structs.extend(d['structures'])
+    client.close()
 
     structs = list(set(structs))
     structs.sort()
     return structs
+
+
+def get_all_networks():
+    client, circ = meth.connect_mongo()
+    data = circ.find({}, {'name': 1})
+    networks = []
+    for d in data:
+        networks.append(d['name'])
+    client.close()
+
+    networks.sort()
+    return networks
