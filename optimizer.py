6eb16bf65e0d3cd21b2ff56a7846d165a0e63e4a
EarthSquirrel <britneyandcaddy1@gmail.com>
2019-12-09 06:41:10 -0700

    Printed out scoring information

--- optimizer.py
+++ optimizer.py
@@ -12,47 +12,45 @@ import random
 # scoring matrix for all the features and algs
 # degree, closeness, between, pagerank 
 scoring_mat = {
-    'fast': [3, 2, 2, 2],  # TODO 
-    'accurate': [1, 1, 1, 0], 
+    'fast': [2, 0, 0, 1],
+    'exact': [1, 1, 0, 0], 
     'global': [0, 1, 1, 3], 
-    'diffusion': [0, 1, 2, 3]
 }
 
 
 def negate_scores(scores):
     ma = max(scores)
     new = [ma-x for x in scores]
+    # print(scores)
+    # print(new)
     return new
 
 
 if __name__ == '__main__':
+    # print(sys.argv)
     # pick the speed 
     if sys.argv[1] == '0':  # slow
-        print('Want it slow')
+        print('Scoring for slow')
         ns = negate_scores(scoring_mat['fast'])
         scoring_mat['fast'] = ns
     # pick accurate
     if sys.argv[2] == '0':
-        ns = negate_scores(scoring_mat['accurate'])
-        scoring_mat['accurate'] = ns
+        print('scoring for less exact')
+        ns = negate_scores(scoring_mat['exact'])
+        scoring_mat['exact'] = ns
     # pick global
     if sys.argv[3] == '0':
+        print('scoring for local')
         ns = negate_scores(scoring_mat['global'])
         scoring_mat['global'] = ns
-    # pick diffusion
-    if sys.argv[4] == '0':
-        ns = negate_scores(scoring_mat['diffusion'])
-        scoring_mat['diffusion'] = ns
-
+    
     # sum up the scores for each algorithm
     sums = [0, 0, 0, 0]
     for i in range(0,4):
         for key in list(scoring_mat.keys()):
             sums[i] += scoring_mat[key][i]
-    print(sums)
 
     ma = max(sums)
-
     alg_run = []
     if ma == sums[0]:
         alg_run.append(center.degree_centrality)
@@ -62,11 +60,21 @@ if __name__ == '__main__':
         alg_run.append(center.betweenness_centrality)
     if ma == sums[3]:
         alg_run.append(center.pagerank)
+    
+    # Print statistics
+    f = scoring_mat['fast']
+    e = scoring_mat['exact']
+    g = scoring_mat['global']
+    print('      Degree Close Between PageRank')
+    print('Fast:   {}    {}     {}       {}'.format(f[0], f[1], f[2], f[3]))
+    print('Exact:  {}    {}     {}       {}'.format(e[0], e[1], e[2], e[3]))
+    print('Global: {}    {}     {}       {}'.format(g[0], g[1], g[2], g[3]))
+    print('Sums:   {}    {}     {}       {}'.format(sums[0], sums[1], sums[2], sums[3]))
 
     print('Picking from: ', alg_run)
     if len(alg_run) > 1:
-        alg = alg_run[random.randomint(0, len(alg_run))]
+        alg = alg_run[random.randint(0, len(alg_run)-1)]
     else:
         alg = alg_run[0]
     print('Running: ', alg)
-    alg()
+    # alg()
